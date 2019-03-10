87547902c5ef1f26a8f58c9ee3da12c9a8dafc45
Kenneth Reitz <me@kennethreitz.org>
2019-03-10 13:13:33 -0400

    migrations

--- core/models.py
+++ core/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.utils import timezone
 from taggit.managers import TaggableManager
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField()
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+    tags = TaggableManager()
+
+    def __unicode__(self):
+        return self.title
