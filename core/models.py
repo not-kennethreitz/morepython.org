82fc09268ad6fb9bb52dc7bd827af43f0c16adfc
Kenneth Reitz <me@kennethreitz.org>
2019-03-11 06:41:38 -0400

    migration

--- core/models.py
+++ core/models.py
@@ -1,14 +1,20 @@
+import uuid
+
 from django.db import models
 from django.utils import timezone
+from django.core.validators import URLValidator
 from taggit.managers import TaggableManager
 
 
-# Create your models here.
 class Post(models.Model):
+    """A Post, posted on the site."""
     title = models.TextField()
     text = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
+    uri = models.URLField()
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    published = models.BooleanField(editable=True, default=False)
 
     def __unicode__(self):
         return self.title
